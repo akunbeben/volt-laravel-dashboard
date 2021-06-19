263e1e42212d9f592e56ad87c45fff0bf0a3946b
Benny Rahmat <akunbeben@gmail.com>
2021-06-19 18:17:24 +0800

    [feat]: added assets, enabled all pages, setup webpacks.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome')->name('welcome');
+Route::view('/upgrade-to-pro', 'pages.pro.index')->name('pro');
+
+Route::group(['prefix' => 'authentication', 'as' => 'auth.'], function() {
+    Route::get('/', function() {
+        return redirect()->route('auth.sign-in');
+    })->name('base');
+
+    Route::view('/sign-in', 'pages.authentication.sign-in')->name('sign-in');
+    Route::view('/sign-up', 'pages.authentication.sign-up')->name('sign-up');
+    Route::view('/forgot-password', 'pages.authentication.forgot-password')->name('forgot-password');
+    Route::view('/reset-password', 'pages.authentication.reset-password')->name('reset-password');
+    Route::view('/locked', 'pages.authentication.locked')->name('locked');
+});
+
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function() {
+    Route::view('/', 'pages.dashboard.index')->name('home');
+    Route::view('/transaction', 'pages.transaction.index')->name('transaction');
+    Route::view('/settings', 'pages.settings.index')->name('settings');
+    Route::view('/tables', 'pages.tables.index')->name('tables');
+    Route::group(['prefix' => 'components'], function() {
+        Route::view('/buttons', 'pages.buttons.index')->name('buttons');
+        Route::view('/notifications', 'pages.notifications.index')->name('notifications');
+        Route::view('/forms', 'pages.forms.index')->name('forms');
+        Route::view('/modals', 'pages.modals.index')->name('modals');
+        Route::view('/typography', 'pages.typography.index')->name('typography');
+    });
+});
+Route::group(['prefix' => 'error', 'as' => 'error.'], function() {
+    Route::get('/', function() {
+        return redirect()->route('error.404');
+    })->name('base');
+    
+    Route::view('/404', 'errors.404')->name('404');
+    Route::view('/500', 'errors.500')->name('500');
 });
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,31 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [
         //
-    ]);
+    ])
+    .styles(
+        [
+            "node_modules/sweetalert2/dist/sweetalert2.css",
+            "node_modules/notyf/notyf.min.css",
+        ],
+        "public/css/vendor.css"
+    )
+    .scripts(
+        [
+            "resources/js/vendor.js",
+            "node_modules/@popperjs/core/dist/umd/popper.min.js",
+            "node_modules/bootstrap/dist/js/bootstrap.min.js",
+            "node_modules/onscreen/dist/on-screen.umd.min.js",
+            "node_modules/nouislider/distribute/nouislider.min.js",
+            "node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js",
+            "node_modules/chartist/dist/chartist.min.js",
+            "node_modules/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js",
+            "node_modules/vanillajs-datepicker/dist/js/datepicker.min.js",
+            "node_modules/moment/moment.min.js",
+            "node_modules/notyf/notyf.min.js",
+            "node_modules/simplebar/dist/simplebar.min.js",
+        ],
+        "public/js/vendor.js"
+    ).sourceMaps();
